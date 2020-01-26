423ea11762ea1e9efe4c1a6517b94af94440471f
Hannah Bunce <hannahlbunce@gmail.com>
2020-01-26 21:17:30 +0000

    Create /summary/{year} endpoint

--- boardgame-summary/app.js
+++ boardgame-summary/app.js
@@ -19,3 +19,24 @@ exports.summaryHandler = async (event, context) => {
 
   return response
 }
+
+exports.summaryByYearHandler = async (event, context) => {
+  const { year } = event.pathParameters
+  const selectedYear = summaryData.byYear.filter(item => item.dateCode === year)[0]
+  try {
+    // const ret = await axios(url);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: selectedYear,
+        currentYear: year
+        // location: ret.data.trim()
+      })
+    }
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+
+  return response
+}
