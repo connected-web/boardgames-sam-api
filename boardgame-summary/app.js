a4929bfc87cd15c437547ef6ea3da491e6a59abe
calisaurus <github@calisaurus.net>
2021-12-01 21:55:51 +0000

    Install AWS SDK and create object in new bucket

--- boardgame-summary/app.js
+++ boardgame-summary/app.js
@@ -1,3 +1,6 @@
+const { PutObjectCommand } = require('@aws-sdk/client-s3')
+const { s3Client } = require('./src/s3Client.js')
+
 const summaryData = require('./data/boardgame-summaries.json')
 
 let response
@@ -43,13 +46,34 @@ exports.summaryByYearHandler = async (event, context) => {
 
 exports.playDataHandler = async (event, context) => {
   let payload
-  let action = {
+  const action = {
     type: 'storePlayData',
     date: new Date()
   }
   if (event.body) {
     payload = JSON.parse(event.body)
-    
+  }
+
+  // Set the parameters
+  const params = {
+    Bucket: 'boardgames-tracking', // The name of the bucket. For example, 'sample_bucket_101'.
+    Key: '2021-11-30-test.json', // The name of the object. For example, 'sample_upload.txt'.
+    Body: JSON.stringify(payload, null, 2) // The content of the object. For example, 'Hello world!".
+  }
+
+  try {
+    const results = await s3Client.send(new PutObjectCommand(params))
+    console.log(
+      'Successfully created ' +
+        params.Key +
+        ' and uploaded it to ' +
+        params.Bucket +
+        '/' +
+        params.Key
+    )
+    return results // For unit tests.
+  } catch (err) {
+    console.log('Error', err)
   }
 
   try {
