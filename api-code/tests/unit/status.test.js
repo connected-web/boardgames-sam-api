2027e9d4235b32ce25f0284cbcd6a8e5100144a3
John Beech <johnbeech.github@mkv25.net>
2021-12-28 15:42:36 +0000

    Update samconfig.toml from main branch

--- api-code/tests/unit/status.test.js
+++ api-code/tests/unit/status.test.js
@@ -1,7 +1,10 @@
+const path = require('path')
+const asyncFs = require('fs/promises')
 const { expect } = require('chai')
 const logs = require('./helpers/logs')
 const app = require('../../app.js')
 const { modifyInterfaces, resetInterfaces } = require('../../src/helpers/interfaces')
+const packageJson = require('../../package.json')
 
 describe('Status Endpoint', () => {
   before(() => {
@@ -21,6 +24,15 @@ describe('Status Endpoint', () => {
 
   it('should report information from package JSON and the current date/time', async () => {
     const actual = await app.default_status({ some: 'event data' })
+
+    let currentBranch = 'unknown'
+    const currentBranchFilepath = path.join(__dirname, '../../current-branch.txt')
+    try {
+      currentBranch = process.env.CURRENT_BRANCH || (await asyncFs.readFile(currentBranchFilepath, 'utf8')).trim()
+    } catch (ex) {
+      console.error('Status Endpoint; unable to read', currentBranchFilepath, '- check if build command executed correctly.')
+    }
+
     const expected = {
       statusCode: 200,
       headers: {
@@ -29,11 +41,15 @@ describe('Status Endpoint', () => {
       },
       body: JSON.stringify({
         name: 'boardgames',
-        version: '1.0.0',
-        description: 'Boardgame Summary API',
+        version: packageJson.version,
+        description: 'An API for summarising boardgame play stats using AWS SAM.',
+        currentBranch,
         currentDate: '2021-12-05T19:19:23.335Z'
       })
     }
+    const actualBody = JSON.parse(actual.body)
+    const expectedBody = JSON.parse(expected.body)
+    expect(actualBody).to.deep.equal(expectedBody)
     expect(actual).to.deep.equal(expected)
     expect(logs.get()).to.deep.equal([
       ['log', 'Status endpoint', new Date('2021-12-05T19:19:23.335Z'), { some: 'event data' }]
