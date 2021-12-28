c709255e3d7bcec39f4a109be6685c371016cbbe
John Beech <johnbeech.github@mkv25.net>
2021-12-28 16:39:07 +0000

    Remove ddbTable index class

--- api-code/src/endpoints/users/get.js
+++ api-code/src/endpoints/users/get.js
@@ -2,21 +2,14 @@ const HTTP_CODES = require('../../helpers/httpCodes')
 const { successResponse, errorResponse } = require('../../helpers/responses')
 
 async function handler (event, context) {
-  let genericDDBTable, getItem
   try {
-    genericDDBTable = require('../../generic/ddbTable')
+    getItem = require('../../generic/ddbTable/getItem')
   } catch (ex) {
     errorResponse(HTTP_CODES.serverError, 'Error loading generic/ddbTable;', ex.message)
   }
 
   try {
-    getItem = genericDDBTable('boardgames-api').getItem
-  } catch (ex) {
-    errorResponse(HTTP_CODES.serverError, 'Error generating genericDDBTable.getItem handler for boardgames-api;', ex.message)
-  }
-
-  try {
-    const response = getItem(event, context)
+    const response = getItem('boardgames-api', event, context)
     successResponse({ response })
   } catch (ex) {
     errorResponse(HTTP_CODES.serverError, 'Error calling genericDDBTable.getItem handler;', ex.message)
