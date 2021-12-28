4317a8e6fd031e9272f6d8c94a439cc8e9e66dd8
John Beech <johnbeech.github@mkv25.net>
2021-12-28 12:05:51 +0000

    Add 'currentBranch' property to status endpoint

--- api-code/src/endpoints/status.js
+++ api-code/src/endpoints/status.js
@@ -2,14 +2,27 @@ const interfaces = require('../helpers/interfaces')
 const { successResponse } = require('../helpers/responses')
 const packageJson = require('../../package.json')
 
+const path = require('path')
+const asyncFs = require('fs/promises')
+
 async function handler (event, context) {
   const { console, now } = interfaces.get()
   const { name, version, description } = packageJson
   console.log('Status endpoint', now(), event)
+
+  let currentBranch = 'unknown'
+  const currentBranchFilepath = path.join(__dirname, '../../current-branch.txt')
+  try {
+    currentBranch = (await asyncFs.readFile(currentBranchFilepath, 'utf8')).trim()
+  } catch (ex) {
+    console.error('Status Endpoint; unable to read', currentBranchFilepath, '- check if build command executed correctly.')
+  }
+
   return successResponse({
     name,
     version,
     description,
+    currentBranch,
     currentDate: now().toISOString()
   })
 }
--- api-code/tests/unit/status.test.js
+++ api-code/tests/unit/status.test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai')
 const logs = require('./helpers/logs')
 const app = require('../../app.js')
 const { modifyInterfaces, resetInterfaces } = require('../../src/helpers/interfaces')
+const asyncExec = require('./helpers/asyncExec')
 const packageJson = require('../../package.json')
 
 describe('Status Endpoint', () => {
@@ -22,6 +23,7 @@ describe('Status Endpoint', () => {
 
   it('should report information from package JSON and the current date/time', async () => {
     const actual = await app.default_status({ some: 'event data' })
+    const currentBranch = (await asyncExec('git rev-parse --abbrev-ref HEAD')).stdout.trim()
     const expected = {
       statusCode: 200,
       headers: {
@@ -32,9 +34,13 @@ describe('Status Endpoint', () => {
         name: 'boardgames',
         version: packageJson.version,
         description: 'Boardgame Summary API',
+        currentBranch,
         currentDate: '2021-12-05T19:19:23.335Z'
       })
     }
+    const actualBody = JSON.parse(actual.body)
+    const expectedBody = JSON.parse(expected.body)
+    expect(actualBody).to.deep.equal(expectedBody)
     expect(actual).to.deep.equal(expected)
     expect(logs.get()).to.deep.equal([
       ['log', 'Status endpoint', new Date('2021-12-05T19:19:23.335Z'), { some: 'event data' }]
