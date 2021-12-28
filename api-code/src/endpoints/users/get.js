839375dc54bf02f40852ee317e04192012c1ae04
John Beech <johnbeech.github@mkv25.net>
2021-12-28 17:03:39 +0000

    Attempt to narrow down failing code

--- api-code/src/endpoints/users/get.js
+++ api-code/src/endpoints/users/get.js
@@ -9,12 +9,16 @@ async function handler (event, context) {
     errorResponse(HTTP_CODES.serverError, 'Error loading generic/ddbTable;', ex.message)
   }
 
+  return successResponse({ getItem: typeof getItem })
+
+  /*
   try {
     const response = await getItem('boardgames-api', event, context)
     successResponse({ response })
   } catch (ex) {
     errorResponse(HTTP_CODES.serverError, 'Error calling genericDDBTable.getItem handler;', ex.message)
   }
+  */
 }
 
 handler.routeName = 'Get User'
